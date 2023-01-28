eac677d02697a5b7d79e1d804a614fac036dec01
Mattskywalker <mateusejesus@hotmail.com.br>
2023-01-28 02:47:46 -0300

    app contexts

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import AppBar from './src/layouts/AppBar';
 import { NativeBaseProvider } from 'native-base';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import TabFooter from './src/layouts/TabFooter';
+import UserProductsProvider from './src/context/UserProductsProvider';
 import ProductsProvider from './src/context/ProductsProvider';
 
 const Tab = createBottomTabNavigator();
@@ -16,26 +17,28 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <ProductsProvider>
-      <NativeBaseProvider>
-        <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen
-              name='Itens' component={ListItens}
-              options={{
-                header: () => <AppBar />,
-                tabBarIcon: (props) => <Icon color={props.color} style={props} size={30} name='list-unordered' />,
-              }} />
-            <Tab.Screen
-              name='UserItens'
-              component={UserItens}
-              options={{
-                header: () => <AppBar />,
-                tabBarIcon: (props) => <Icon color={props.color} style={props} size={30} name='checklist' />
-              }}
-            />
-          </Tab.Navigator>
-        </NavigationContainer>
-      </NativeBaseProvider>
+      <UserProductsProvider>
+        <NativeBaseProvider>
+          <NavigationContainer>
+            <Tab.Navigator>
+              <Tab.Screen
+                name='Itens' component={ListItens}
+                options={{
+                  header: () => <AppBar />,
+                  tabBarIcon: (props) => <Icon color={props.color} style={props} size={30} name='list-unordered' />,
+                }} />
+              <Tab.Screen
+                name='UserItens'
+                component={UserItens}
+                options={{
+                  header: () => <AppBar />,
+                  tabBarIcon: (props) => <Icon color={props.color} style={props} size={30} name='checklist' />
+                }}
+              />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </NativeBaseProvider>
+      </UserProductsProvider>
     </ProductsProvider>
   );
 }
