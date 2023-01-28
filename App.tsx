6afbe95aba9c668d3c3e1c7ea4536a54626aa51c
Mattskywalker <mateusejesus@hotmail.com.br>
2023-01-28 20:10:58 -0300

    app

--- App.tsx
+++ App.tsx
@@ -1,18 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Icon from 'react-native-vector-icons/Octicons'
-import { NavigationContainer } from '@react-navigation/native';
 
-import ListItens from './src/views/ListItens';
-import UserItens from './src/views/UserItens';
-import AppBar from './src/layouts/AppBar';
+import { StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { NativeBaseProvider } from 'native-base';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import TabFooter from './src/layouts/TabFooter';
 import UserProductsProvider from './src/context/UserProductsProvider';
 import ProductsProvider from './src/context/ProductsProvider';
 
-const Tab = createBottomTabNavigator();
+import Routes from './src/routes';
 
 export default function App() {
   return (
@@ -20,22 +13,7 @@ export default function App() {
       <UserProductsProvider>
         <NativeBaseProvider>
           <NavigationContainer>
-            <Tab.Navigator>
-              <Tab.Screen
-                name='Itens' component={ListItens}
-                options={{
-                  header: () => <AppBar />,
-                  tabBarIcon: (props) => <Icon color={props.color} style={props} size={30} name='list-unordered' />,
-                }} />
-              <Tab.Screen
-                name='UserItens'
-                component={UserItens}
-                options={{
-                  header: () => <AppBar />,
-                  tabBarIcon: (props) => <Icon color={props.color} style={props} size={30} name='checklist' />
-                }}
-              />
-            </Tab.Navigator>
+            <Routes />
           </NavigationContainer>
         </NativeBaseProvider>
       </UserProductsProvider>
