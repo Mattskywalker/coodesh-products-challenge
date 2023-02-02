cd0d8f24362526319e0b7642413cce9efe74eaf4
Mattskywalker <mateusejesus@hotmail.com.br>
2023-02-02 13:36:30 -0300

    .env

--- src/api/firebase/index.ts
+++ src/api/firebase/index.ts
@@ -2,22 +2,29 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  APIKEY,
+  APPID,
+  AUTHDOMAIN,
+  DATABASEURL,
+  MESSAGINGSENDERID,
+  PROJECTID,
+  STORAGEBUCKET
+} from '@env';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC2nu_WI5MMpl66c4FzKG47Q6rCY8vtMGg",
-  authDomain: "products-coodesh-e062a.firebaseapp.com",
-  databaseURL: "https://products-coodesh-e062a-default-rtdb.firebaseio.com",
-  projectId: "products-coodesh-e062a",
-  storageBucket: "products-coodesh-e062a.appspot.com",
-  messagingSenderId: "709430790426",
-  appId: "1:709430790426:web:4020d1e12a98d99f2a84a3"
+  apiKey: APIKEY,
+  authDomain: AUTHDOMAIN,
+  databaseURL: DATABASEURL,
+  projectId: PROJECTID,
+  storageBucket: STORAGEBUCKET,
+  messagingSenderId: MESSAGINGSENDERID,
+  appId: APPID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig); 
+const app = initializeApp(firebaseConfig);
 
 export const dataBase = getFirestore(app);
 export const firebaseStorage = getStorage(app, "gs://products-coodesh-e062a.appspot.com");
